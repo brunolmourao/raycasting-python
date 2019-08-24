5c2553a4ae34ebdc6430732cb635d6409f331acc
Marcos C Lima <marcos.lima.crn@gmail.com>
2019-08-24 10:36:24 -0300

    Add properties to class Plane

--- Plane.py
+++ Plane.py
@@ -1,7 +1,27 @@
 """"
 Classe do Objeto Plano
+A classe Plane representa um plano onde:
+p0 é um	ponto qualquer do plano;
+ppl é um ponto específico (conhecido) do plano; e
+vet_n é	o vetor	unitário perpendicular ao plano, isto é, a todos os	vetores paralelos ao plano.
 """
 
 
 class Plane:
-    pass
+    # Método Construtor
+    def __init__(self, p0, ppl, vet_n):
+        self.__p0 = p0
+        self.__ppl = ppl
+        self.__vet_n = vet_n
+
+    # Método getters
+    @property
+    def p0(self):
+        return self.__p0
+
+    def ppl(self):
+        return self.__ppl
+
+    @property
+    def vet_n(self):
+        return self.__vet_n
