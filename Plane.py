829dc482897204073f34ca36ab7c26e78e16b93d
Marcos C Lima <marcos.lima.crn@gmail.com>
2019-08-24 10:48:34 -0300

    Add properties to class Cillinder

--- Plane.py
+++ Plane.py
@@ -1,7 +1,7 @@
 """"
 Classe do Objeto Plano
 A classe Plane representa um plano onde:
-p0 é um	ponto qualquer do plano;
+p é um	ponto qualquer do plano;
 ppl é um ponto específico (conhecido) do plano; e
 vet_n é	o vetor	unitário perpendicular ao plano, isto é, a todos os	vetores paralelos ao plano.
 """
@@ -9,16 +9,17 @@ vet_n é	o vetor	unitário perpendicular ao plano, isto é, a todos os	vetores p
 
 class Plane:
     # Método Construtor
-    def __init__(self, p0, ppl, vet_n):
-        self.__p0 = p0
+    def __init__(self, p, ppl, vet_n):
+        self.__p = p
         self.__ppl = ppl
         self.__vet_n = vet_n
 
     # Método getters
     @property
-    def p0(self):
-        return self.__p0
+    def p(self):
+        return self.__p
 
+    @property
     def ppl(self):
         return self.__ppl
 
