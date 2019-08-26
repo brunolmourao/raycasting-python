ecb758d076b84d46a9d86c90559f811da4a04d5a
Matheus Silva Sousa <sousasilvamatheus@gmail.com>
2019-08-26 01:17:20 -0300

    Trocando forma de raiz quadrada no cálculo das raízes da equação

--- auxiliar/QuadraticOperations.py
+++ auxiliar/QuadraticOperations.py
@@ -2,6 +2,8 @@
 Classe que calcula operações básicas para os problemas, como delta, normalização, dentre outras
 
 """
+import numpy as np
+
 
 def delta(a, b, c):
     return b * b - 4 * a * c
@@ -13,5 +15,5 @@ def roots(a, b, c):
     elif d == 0:
         return (-1) * b / 2 * a
     else:
-        return [((-1) * b + delta(a, b, c) ** 0.5) / 2 * a,
-                ((-1) * b + delta(a, b, c) ** 0.5) / 2 * a]
+        return [((-1) * b + np.sqrt(delta(a, b, c))) / 2 * a,
+                ((-1) * b + np.sqrt(delta(a, b, c))) / 2 * a]
