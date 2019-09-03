6565d5552d10a5c4b3659c153f5cd39362bd1e35
Bruno <brunolmourao10@gmail.com>
2019-09-02 22:42:00 -0300

    Consertando Contador

--- estruturaDeDados/Point.py
+++ estruturaDeDados/Point.py
@@ -5,7 +5,7 @@ class Point:
     contador = 0
 
     def __init__(self, x, y, z):
-        self.id = self.contador + 1
+        self.id = Point.contador + 1
         self.__x = x
         self.__y = y
         self.__z = z
