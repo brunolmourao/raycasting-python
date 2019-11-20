d1f3fd2ec2d09ba3b0cea052e8f58761cae5f0da
Bruno Mourao <brunolmourao10@gmail.com>
2019-11-20 13:02:27 -0300

    Adicionando Classe Material

--- utils/Material.py
+++ utils/Material.py
@@ -1,3 +1,17 @@
 class Material:
-    def __init__(self):
-      pass
+    def __init__(self, mat_dif, mat_sp, mat_amb):
+        self.__mat_dif = mat_dif
+        self.__mat_sp = mat_sp
+        self.__mat_amb = mat_amb
+
+    @property
+    def mat_dif(self):
+        return self.__mat_dif
+
+    @property
+    def mat_sp(self):
+        return self.__mat_sp
+
+    @property
+    def mat_amb(self):
+        return self.__mat_amb
